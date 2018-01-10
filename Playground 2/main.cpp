3a922518850fc7d4a41cd8e2017dcf6b88496b30
Takáts Bálint <takats.balint1@gmail.com>
2018-01-10 20:14:43 +0100

    Initial Commit

---
+++ Playground 2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Playground 2
+//
+//  Created by Takáts Bálint on 2018. 01. 10..
+//  Copyright © 2018. Takáts Bálint. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
