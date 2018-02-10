14272464dd9b0b2aec1ff9f489dad738654df3a2
Takáts Bálint <takats.balint1@gmail.com>
2018-02-10 09:33:24 +0100

    Passes tests 100% (hurray!)

--- Playground 2/main.cpp
+++ Playground 2/main.cpp
@@ -7,6 +7,7 @@
 //
 
 #include <iostream>
+#include <algorithm>
 #include <vector>
 #include <list>
 #include <chrono>
@@ -22,61 +23,67 @@ public:
     int bax, bay, jfx, jfy;
 };
 long long int sum = 0, diff = 0;
-unordered_multimap<int,int> map_x;
-unordered_multimap<int,int> map_y;
+int a,b,c,d;
+unordered_map<int,vector<pair<int,int>>> map_x; // also x cord
+unordered_map<int,vector<pair<int,int>>> map_y; // also x cord
 int builds;
 rect arr [100000];
 int main() {
     // insert code here...
     //std::cout << "Hello, World!\n";
     //ifstream in("/Users/takatsbalint/Documents/Projects/Playground 2/Playground 2/base.txt");
-    auto b = chrono::high_resolution_clock::now();
+    auto be = chrono::high_resolution_clock::now();
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     //map_x.reserve(builds);
-    cin >> builds;
-    map_x.reserve(builds);
-    map_y.reserve(builds);
-    for(int i = 0;i < builds; i++){
-        int a,b,c,d;
+    cin >> builds;;
+    //map_x.reserve(builds);
+    for(int i = 0; i < builds; i++){
         cin >> a >> b >> c >> d;
         sum += (c - a) * 2 + (d - b) * 2;
-        arr[i] = {a,b,c,d};
-        map_x.insert({a,i});
-        map_y.insert({b,i});
+        //arr[i] = {a,b,c,d};
+        //auto p = map_x.find(a);
+        map_x[a].push_back({b,d});
+        map_x[c].push_back({b,d});
+        
+        map_y[b].push_back({a,c});
+        map_y[d].push_back({a,c});
     }
-    for (int i = 0; i < builds; i++){
-        auto r = map_x.equal_range(arr[i].jfx);
-        for(auto j = r.first; j != r.second;j++){
-            int mini = min(arr[i].jfy,arr[j->second].jfy);
-            int maxi = max(arr[i].bay,arr[j->second].bay);
-            if (mini-maxi>0 ){
-                diff += mini-maxi;
+    for(auto i: map_x){
+        sort(i.second.begin(),i.second.end());
+        for(int j = 0; j < i.second.size()-1;j++){
+            for(int k = j + 1; k < i.second.size();k++){
+                int mini = min(i.second[j].second,i.second[k].second);
+                int maxi = max(i.second[j].first,i.second[k].first);
+                if (mini-maxi > 0 ){
+                    diff += mini-maxi;
+                }
+                if (i.second[j].second < i.second[k].second){
+                    j = k - 1;
+                    break;
+                }
+                j = k;
             }
-            /*if ((arr[i].bay <= arr[j->second].bay && arr[i].jfy >= arr[j->second].bay) ||
-                (arr[i].bay <= arr[j->second].jfy && arr[i].jfy >= arr[j->second].jfy) ||
-                (arr[i].bay >=arr[j->second].bay && arr[i].jfy <= arr[j->second].jfy) ){
-                //cout <<min(arr[i].jfy,arr[j->second].jfy)-max(arr[i].bay,arr[j->second].bay)<< "\n";
-                diff += min(arr[i].jfy,arr[j->second].jfy)-max(arr[i].bay,arr[j->second].bay);
-            }*/
         }
-        auto r2 = map_y.equal_range(arr[i].jfy);
-        for(auto j = r2.first; j != r2.second;j++){
-            //cout <<arr[i].bax <<" "<<arr[i].jfx <<" "<<arr[j->second].bax<<" "<<arr[j->second].jfx<< "\n";
-            int mini = min(arr[i].jfx,arr[j->second].jfx);
-            int maxi = max(arr[i].bax,arr[j->second].bax);
-            if (mini-maxi>0 ){
-                diff += mini-maxi;
+    }
+    for(auto i: map_y){
+        sort(i.second.begin(),i.second.end());
+        for(int j = 0; j < i.second.size()-1;j++){
+            for(int k = j + 1; k < i.second.size();k++){
+                int mini = min(i.second[j].second,i.second[k].second);
+                int maxi = max(i.second[j].first,i.second[k].first);
+                if (mini-maxi > 0 ){
+                    diff += mini-maxi;
+                }
+                if (i.second[j].second < i.second[k].second){
+                    j = k - 1;
+                    break;
+                }
+                j = k;
             }
-           // if ((arr[i].bax <= arr[j->second].bax && arr[i].jfx >= arr[j->second].bax) ||
-                //(arr[i].bax <= arr[j->second].jfx && arr[i].jfx >= arr[j->second].jfx) ||
-                //(arr[i].bax >= arr[j->second].bax && arr[i].jfx <= arr[j->second].jfx) ){
-               // cout << min(arr[i].jfx,arr[j->second].jfx)-max(arr[i].bax,arr[j->second].bax) << "\n";
-                //diff += min(arr[i].jfx,arr[j->second].jfx)-max(arr[i].bax,arr[j->second].bax);
-           // }
         }
     }
     auto e = chrono::high_resolution_clock::now();
-    //cout << chrono::duration_cast<chrono::milliseconds>(e-b).count()<< "\n";
+    //cout << chrono::duration_cast<chrono::milliseconds>(e-be).count()<< "\n";
     cout<< sum - diff * 2 << "\n";
 }
